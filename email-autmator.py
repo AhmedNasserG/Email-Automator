33b1a6ad93624e388a81bd3713271eb24f45e99f
Ahmed Nasser <ahmednasser217217@gmail.com>
2021-02-12 18:24:36 +0200

    Add: send mail method

--- email-autmator.py
+++ email-autmator.py
@@ -1,5 +1,7 @@
 import os
 import getpass
+import smtplib 
+
 
 from rich.prompt import Confirm
 
@@ -8,17 +10,32 @@ from rich.prompt import Confirm
 def get_credenalties():
     ''' get login credenalties '''
     if not os.path.isfile(".credenalites"):
-        username = input("Enter Sender Email : ")
+        sender_email = input("Enter Sender Email : ")
         password = getpass.getpass(prompt="Enter Password : ")
         remember_me = Confirm.ask("Remember me ?")
         if remember_me:
             with open(".credenalites", "w") as f:
-                f.write(username+"\n"+password)
+                f.write(sender_email+"\n"+password)
     else:
         with open(".credenalites", "r") as f:
             lines = f.readlines()
-            username = lines[0].strip()
+            sender_email = lines[0].strip()
             password = lines[1].strip()
-    return username, password
+    return sender_email, password
 
-get_credenalties()
+
+def send_mail(receiver_emails, email_content):
+    ''' send a mail to a group of emails '''
+    sender_email, password = get_credenalties()
+    s = smtplib.SMTP("smtp.gmail.com", 587) 
+    s.starttls() 
+    s.login(sender_email, password) 
+    for receiver_email in receiver_emails:
+        s.sendmail(sender_email, receiver_email, email_content) 
+    s.quit()
+
+
+# test
+receiver_emails, email = ["ahmednasser21731@yahoo.com", "ahmednasser217217@gmail.com"], "This is a test from python script"
+send_mail(receiver_emails, email)
+print("done")
