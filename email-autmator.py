a43491c85c26b3c3ee95fec4fe4a729f5afda5a4
Ahmed Nasser <ahmednasser217217@gmail.com>
2021-02-14 02:42:43 +0200

    Add: show checklist method

--- email-autmator.py
+++ email-autmator.py
@@ -10,10 +10,13 @@ from email.mime.text import MIMEText
 
 from PyInquirer import prompt
 
-from rich.prompt import Confirm
+from rich.prompt import Confirm, Prompt 
 from rich import print
 from rich.console import Console
 from rich.panel import Panel
+
+# TODO: Fix can't close the script when the menu is shown
+# TODO: Add Auth method
 
 console = Console()
 
@@ -29,6 +32,18 @@ def showMenu(title, options):
         if len(list(terminal_menu.values())) != 0:
             break
     return list(terminal_menu.values())[0]
+
+def show_checklist(title, options):
+    questions = [
+    {
+        'type': 'checkbox',
+        'message': title,
+        'name': 'emails',
+        'choices': [dict(zip(['name'],[op])) for op in options]
+    }
+    ]
+    choices = prompt(questions)
+    return [options.index(i) for i in choices['emails']]
 
 def get_credenalties():
     ''' get login credenalties '''
@@ -51,7 +66,7 @@ def get_mail_content():
     SUPPORTED_EXTENTSION = {"txt" : "plain","html" :"html"}
     supported_files = [f for f in os.listdir() if f.split(".")[-1] in SUPPORTED_EXTENTSION.keys()]
     if len(supported_files) == 0:
-        console.print(Panel(f'''[red]Sorry, There are not any Supported Files in Email-Autmator Directory [/red]
+        console.print(Panel(f'''[red]Sorry, There are not any Supported Files in Current Directory [/red]
 We support {str(list(SUPPORTED_EXTENTSION.keys()))[1:-1]}.
 If you need other types add an issue on Github: https://github.com/AhmedNasserG and we will add them ASAP
 Please Add One And Try Again''', style="bold"))
@@ -62,6 +77,8 @@ Please Add One And Try Again''', style="bold"))
         message = MIMEMultipart()
         message.attach(MIMEText(f.read(), SUPPORTED_EXTENTSION[extentsion]))
     return message
+
+
 
 def send_mail(receiver_emails, message):
     ''' send a mail to a group of emails '''
@@ -75,7 +92,9 @@ def send_mail(receiver_emails, message):
 
 
 # test
-receiver_emails, email = ["ahmednasser21731@yahoo.com"], get_mail_content()
-send_mail(receiver_emails, email)
+# receiver_emails, email = ["ahmednasser21731@yahoo.com"], get_mail_content()
+# send_mail(receiver_emails, email)
 # print("done")
 # get_mail_content()
+# get_receiver_emails_manual()
+# show_checklist("test", ["op1","op2","op3","op4"])
